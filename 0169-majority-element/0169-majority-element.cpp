4f394d7abc42e596a4c81c9c3606b9dd67838c16
Kirti Pant <168737236+KirtiPant@users.noreply.github.com>
2025-08-23 14:55:29 +0530

    Time: 0 ms (100%), Space: 28.1 MB (86.87%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int freq=0; // koi element nahi h bas 0 h uske andr
+        int ans=0;
         int n=nums.size();
-        for(int i : nums){
-            int fre = 0;
-            for(int el : nums){
-                if(el == i){
-                    fre ++;
-                }
+        for(int i=0;i<n;i++){
+            if(freq == 0){
+                ans=nums[i];//ye answer ho gaya agar freq 0 h to
             }
-            if(fre>n/2){
-                return i;
+            if(ans == nums[i]){ // angr jo answer ke andr h nums[i] uske equal koi aur variable bhi h to batao
+            freq++;
+            }
+            else{
+                freq--;//-- isliye kiya kyuki har baar value ko reset ni krna padega simply 0 hi jaegi vapis.
             }
         }
-        return -1;
+        return ans;
     }
 };
